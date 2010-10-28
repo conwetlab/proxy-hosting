3e7e51b3915e2bcb09ae8e9b26b8ef698e5e43a6
Javier Lopez <jlopez@fi.upm.es>
2010-10-28 17:13:43 +0200

    Remove folder when removing user

--- ftpd/models.py
+++ ftpd/models.py
@@ -1,7 +1,7 @@
 import os
 
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.contrib.auth.models import User
 from django.conf import settings
 
@@ -12,4 +12,11 @@ def create_user_dir(sender, **kwargs):
         if not os.path.exists(home_dir):
             os.mkdir(home_dir)
 
+def remove_user_dir(sender, **kwargs):
+    user = kwargs['instance']
+    home_dir = os.path.join(settings.FTP_BASE, user.username)
+    if os.path.exists(home_dir):
+        os.rmdir(home_dir)
+
 post_save.connect(create_user_dir,sender=User)
+post_delete.connect(remove_user_dir, sender=User)
