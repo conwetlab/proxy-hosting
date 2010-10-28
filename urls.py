f6731772545827671b3a60ac2597e7b59d8adead
Javier Lopez <jlopez@fi.upm.es>
2010-10-28 13:28:12 +0200

    Serving user files

--- urls.py
+++ urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from settings import FTP_BASE
 admin.autodiscover()
 
 urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'^(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': FTP_BASE, 'show_indexes': True }),
 )
