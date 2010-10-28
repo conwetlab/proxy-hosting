b60586c570ba5128ff0f82e91df6d5ba17d8abe4
Javier Lopez <jlopez@fi.upm.es>
2010-10-28 13:06:49 +0200

    Automatic user dir creation

--- ftpd/models.py
+++ ftpd/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+import os
 
-# Create your models here.
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.conf import settings
+
+def create_user_dir(sender, **kwargs):
+    username = kwargs['instance'].username
+    home_dir = os.path.join(settings.FTP_BASE, username)
+    os.mkdir(home_dir)
+
+post_save.connect(create_user_dir,sender=User)
